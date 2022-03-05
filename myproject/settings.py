cdf47897d97dbefd440613d23b0c99c3e9195668
Mahipal Singh <msingh@repay.com>
2022-03-05 13:01:57 +0530

    - Disabled Debugging - This will fix a security issue.

--- myproject/settings.py
+++ myproject/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-n_*%vw5^uwo22mpfwe++b2udc!!$^+q1u9tv@ns%=154+c-68k'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
